b362995c070184d830bdced884996a3cd2e78e3e
SAMIAABBA <melinasamia1984@gmail.com>
2021-03-31 09:38:07 -0400

    added search page feature code

---
+++ src/main/java/com/bankOfamiricabase/SearchPage.java
@@ -0,0 +1,11 @@
+package com.bankOfamiricabase;
+
+public class SearchPage {
+
+	public void searchPage() {
+		System.out.println("this is search page");
+	}
+	public void searchSet() {
+		System.out.println("this is set page");
+	}
+}
